f542d0b9a2486efc26c7517554c69b8eaa34be4f
Deshalin Naicker <131306172+ST10287087@users.noreply.github.com>
2023-09-29 14:06:31 +0200

    Update HarbingerBossFight.java

--- src/programmingassignmentanswer2/HarbingerBossFight.java
+++ src/programmingassignmentanswer2/HarbingerBossFight.java
@@ -1,40 +1,43 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+
+
+ @author ST10287087
  */
 package programmingassignmentanswer2;
 
 public class HarbingerBossFight {
     public void start() {
-        // Create player character
+        // Active playerrr
         Attackker player = new ActivePlayer("Player", 60, 5);
 
-        // Create the boss
+        // startind boss
         Attackker boss = new Harbinger("Harbinger", 45);
 
         System.out.println("WELCOME TO THE FATUI HARBINGER BOSS BATTLE");
         System.out.println("IF YOU WANT TO LIVE DEFEAT THE BOSS");
 
-        // Game loop
-        while (((CharacterMoves) player).isAlive() && ((CharacterMoves) boss).isAlive()) {
+        // this loops the game to see who will win
+        while (((CharacterMoves) player).isAlive() && ((CharacterMoves) boss).isAlive()) {//shows use the current health points
             System.out.println(((CharacterMoves) player).getName() + " - HealthPoints = " + ((CharacterMoves) player).getHealth());
             System.out.println(((CharacterMoves) boss).getName() + " - HealthPoints  =  " + ((CharacterMoves) boss).getHealth());
 
-            // Player's turn
+            // your characters turn
             player.attack((CharacterMoves) boss);
 
-            // Boss's turn
+            // HArbingers turn
             if (((CharacterMoves) boss).isAlive()) {
                 boss.attack((CharacterMoves) player);
             }
         }
 
-        // Determine the winner
-        if (((CharacterMoves) player).isAlive()) {
-            System.out.println("I was meant for something more than this... the last words of the harbinger before they perished, CONGRATS COMRADE!");
-            System.out.println("now you get the chance to win 1 of 5 prizes may the odds be in your favor, goodluck");
+        
+        if (((CharacterMoves) player).isAlive()) {//checks if player is alive or not
+            System.out.println("I was meant for something more than this... the last words of the harbinger before they perished, CONGRATS COMRADE!");//bosses defeated message
+            System.out.println("now you get the chance to win 1 of 5 prizes may the odds be in your favor, goodluck");//informas them of the lucky victory draw
             String[] Prizes;
-            Prizes = new String[6];
+            Prizes = new String[6]; //if the player wins they have a chnace of getting 1 of 5 rewards!
             Prizes[0] = "100000X Gold coins Obtained!";
             Prizes[1] = "YOU GOT NOTHING";
             Prizes[2] = "YOU GOT NOTHING!";
@@ -43,10 +46,10 @@ public class HarbingerBossFight {
             Prizes[5] = "100000X Gold coins Obtained!";
             
             int num = (int) (Math.random()* 6);
-            System.out.println("Congrasts!" + Prizes[num]);
+            System.out.println("Congrasts!" + Prizes[num]);//random number generator to choose a prize
 
         } else {
-            System.out.println("You faced the lighting directly ,hmph commendable. as you hear the last words before you perished.Game over! ");
+            System.out.println("You faced the lighting directly ,hmph commendable. as you hear the last words before you perished.Game over! ");//bosses victory message
         }
     }  
 }
