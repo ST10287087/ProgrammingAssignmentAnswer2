a276cb62afcddc9aa7bdd89168406ad55de93f90
Deshalin Naicker <131306172+ST10287087@users.noreply.github.com>
2023-09-29 13:59:07 +0200

    Update ActivePlayer.java

--- src/programmingassignmentanswer2/ActivePlayer.java
+++ src/programmingassignmentanswer2/ActivePlayer.java
@@ -8,15 +8,15 @@ import java.util.Random;
 
 /**
  *
- * @author User
+ * @author ST10287087
  */
 public class ActivePlayer extends CharacterMoves implements Attackker {
   
     public ActivePlayer(String name, int health, int damage) {
-        super(name, health, damage);
+        super(name, health, damage);//players details
     }
 
-    @Override
+    @Override//determines the players power
      public void attack(CharacterMoves target) {
         int damageDealt = new Random().nextInt(getDamage()) + 1;
         System.out.println(getName() + " attacks " + target.getName() + " for " + damageDealt + " damage.");
